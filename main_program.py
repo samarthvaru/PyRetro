379b5e40898b81d6cb888267bf36a6817715f8be
samarthvaru <63781024+samarthvaru@users.noreply.github.com>
2023-03-26 20:53:24 -0300

    updated main.py

--- main_program.py
+++ main_program.py
@@ -40,62 +40,67 @@ def run_tetris():
 def run_tictactoe():
     os.system('tictactoe.py')
     
+def memory_game():
+    os.system('memorygame.py')
 
 def display_text():
    global entry
-   string= entry.get()
+   string= "Welcome to Py-Retro "+ entry.get()
    label.configure(text=string)
    entry.delete(0, END)
    btn_inp.after(1, btn_inp.destroy)
    entry.after(1,entry.destroy)
 
-button_image = PhotoImage(file="images/Tic Tac Toe.png")
 
-
-# create a frame to contain the buttons
-button_frame = Frame(window)
-button_frame.pack()
-button_frame.image = button_image
 
 #Initialize a Label to display the User Input
-label=Label(button_frame, text="", font=("Courier 22 bold"))
+label=Label(window, text="", fg="white", font=("Comic Sans MS",22),bg="#3A928E")
 # label.grid(column=1, row=0)
 label.place(relx = 0.5,
-                   rely = 0.5,
-                   anchor = 'center')
+                   rely = 0.49,
+                   anchor = CENTER)
 # label.pack()
 
 #Create an Entry widget to accept User Input
-entry= Entry(button_frame, width= 40, bd=0, highlightthickness=0)
+entry= Entry(window, width= 40, bd=0, highlightthickness=0)
 entry.focus_set()
 entry.pack()
+entry.place(relx=0.5, rely=0.47,anchor=CENTER)
 # entry.pack()
 
 #Create a Button to validate Entry Widget
-btn_inp=Button(button_frame, text= "Okay",width= 20, command= display_text)
+btn_inp=Button(window, text= "Okay",fg="white",width= 20, command= display_text,highlightthickness=0, bg="#3A928E")
 # btn_label.grid(column=1, row=0)
 btn_inp.pack()
+btn_inp.place(relx=0.5, rely=0.52, anchor=CENTER)
 
 
 
 # create 5 buttons in the frame
-button1 = Button(button_frame,text="Snake Game", bd=0, fg="white",font=("Helvetica", 20),command=run_snake,width=20, height=1,highlightthickness=0)
+button1 = Button(window,text="Snake Game", bd=0,fg="white",font=("Comic Sans MS", 17),bg="#5E3174",width= 20,height=1,command=run_snake,highlightthickness=0)
 button1.pack(padx=10, pady=10)
 
-button2 = Button(button_frame, text="Connect four",bd=0, fg="white",font=("Helvetica", 20),command=run_connect_four,width=20, height=1,highlightthickness=0, highlightbackground="white")
+button2 = Button(window, text="Connect four",bd=0, fg="white",font=("Comic Sans MS", 17),bg="#5E3174",width= 20,height=1,command=run_connect_four,highlightthickness=0, highlightbackground="white")
 button2.pack(padx=10, pady=10)
 
-button3 = Button(button_frame, text="SuperHuman",bd=0, fg="white",font=("Helvetica", 20),command=run_superhuman,width=20, height=1,highlightthickness=0, highlightbackground="white")
+button3 = Button(window, text="SuperHuman",bd=0 ,fg="white",font=("Comic Sans MS", 17),bg="#5E3174",width= 20,height=1,command=run_superhuman,highlightthickness=0, highlightbackground="white")
 button3.pack(padx=10, pady=10)
 
-button4 = Button(button_frame, text="Tetris",bd=0, fg="white",font=("Helvetica", 20),command=run_tetris,width=20, height=1,highlightthickness=0, highlightbackground="white")
+button4 = Button(window, text="Tetris",bd=0 ,fg="white",font=("Comic Sans MS", 17),bg="#5E3174",width= 20,height=1,command=run_tetris,highlightthickness=0, highlightbackground="white")
 button4.pack(padx=10, pady=10)
 
-button5 = Button(button_frame, text="Tictactoe",bd=0, image= button_image,fg="white",font=("Helvetica", 20),command=run_tictactoe,highlightthickness=0, highlightbackground="white")
+button5 = Button(window, text="Tic Tac Toe",bd=0,fg="white",font=("Comic Sans MS", 17),bg="#5E3174",width= 20,height=1,command=run_tictactoe,highlightthickness=0, highlightbackground="white")
 button5.pack(padx=10, pady=10)
 
-# center the frame containing the buttons
-button_frame.place(relx=0.5, rely=0.6, anchor="center")
+button6 = Button(window, text="Memory Game",bd=0,fg="white",font=("Comic Sans MS", 17),bg="#5E3174",width= 20,height=1,command=memory_game,highlightthickness=0, highlightbackground="white")
+button6.pack(padx=10, pady=10)
+
+button1.place(relx=0.25, rely=0.60, anchor=CENTER)
+button2.place(relx=0.75, rely=0.60, anchor=CENTER)
+button3.place(relx=0.25, rely=0.70, anchor=CENTER)
+button4.place(relx=0.75, rely=0.70, anchor=CENTER)
+button5.place(relx=0.25, rely=0.80, anchor=CENTER)
+button6.place(relx=0.75, rely=0.80, anchor=CENTER)
 
 
 window.mainloop()
